21bfd9496ddf745776421e8c151b3bec1a410ccb
lefebvre <marie.lefebvre@univ-nantes.fr>
2018-08-01 16:48:38 +0200

    Add Ecsv2compare module

---
+++ launchers/Ecsv2compare.py
@@ -0,0 +1,66 @@
+import os.path
+from subprocess import call
+import logging as log
+import sys
+import glob
+import csv
+import xlsxwriter as xlwt
+import re
+
+class Ecsv2compare:
+
+	def __init__ (self, args):
+		self.check_args(args)
+		self.cmd = []
+		self.create_cmd()
+
+	def create_cmd (self):
+		cmd = 'ecsv2compare.py'
+		for c in self.blast_files:
+			cmd += ' -b ' + str(c)
+		if self.rps_file != '':
+			cmd += ' -r ' + self.rps_file
+		cmd += ' -o ' + self.out
+		log.debug(cmd)
+		self.cmd.append(cmd)
+
+
+	def check_args (self, args: dict):
+		self.execution=1
+		self.sample = args['sample']
+		self.wd = os.getcwd() + '/' + self.sample
+		self.cmd_file = self.wd + '/' + 'ecsv2compare_cmd.txt'
+		if 'out' in args:
+			self.out = self.wd + '/' + args['out']
+		self.blast_files = []
+		for i in range(1, 10, 1):
+			opt_name = 'b' + str(object=i)
+			if opt_name in args:
+				if os.path.exists(self.wd + '/' + args[opt_name]):
+					self.blast_files.append(self.wd + '/' + args[opt_name])
+		if 'r' in args:
+			if os.path.exists(self.wd + '/' + args['r']):
+				self.rps_file = self._check_file(self.wd + '/' + args['r'])
+			else:
+				self.rps_file = ''
+		else:
+			self.rps_file = ''
+		if len(self.blast_files) == 0:
+			self.execution=0
+		if 'sge' in args:
+			self.sge = bool(args['sge'])
+		else:
+			self.sge = False
+		if 'n_cpu' in args:
+			self.n_cpu = str(args['n_cpu'])
+		else:
+			self.n_cpu = '1'
+
+
+	def _check_file (self,f):
+		try:
+			open(f)
+			return f
+		except IOError:
+			print('File not found ' + f)
+			sys.exit(1)
---
+++ tools/ecsv2compare.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+import csv, os, re
+from subprocess import call
+import logging as log
+import argparse
+import sys, getopt
+import glob
+import xlsxwriter as xlwt
+
+
+def main ():
+	options = _set_options()
+	log.debug("Merge csv file ")
+	wb = xlwt.Workbook(options.out)
+	blast_query_id_list = list()
+	for filename in options.blast_files:
+		(f_path, f_name) = os.path.split(filename)
+		(f_short_name_tmp, f_extension) = os.path.splitext(f_name)
+		f_short_name = f_short_name_tmp.replace("scaffold.", "")
+		ws = wb.add_worksheet(f_short_name)
+		spamReader = csv.reader(open(filename, "r"), delimiter="\t")
+		for rowx, row in enumerate(spamReader):
+			cell_format = wb.add_format({})
+			if len(row) > 13 :
+				taxonomy = row[14]
+				if taxonomy.startswith( 'Virus' ):
+					cell_format = wb.add_format({"bg_color":"#CCFFFF"})
+					blast_query_id_list.append(row[1])
+				else:
+					cell_format = wb.add_format({})
+			ws.write_row(rowx,0, row, cell_format)
+		ws.autofilter('A1:P1')
+	
+	if (options.rps_file != ""):
+		for filename in options.rps_file:
+			pattern = r"(.)*Virus(.)*;"
+			regex = re.compile(pattern)
+			(f_path, f_name) = os.path.split(filename)
+			(f_short_name_tmp, f_extension) = os.path.splitext(f_name)
+			f_short_name = f_short_name_tmp.replace("scaffold.", "")
+			ws = wb.add_worksheet(f_short_name)
+			spamReader = csv.reader(open(filename, "r"), delimiter="\t")
+			for rowx, row in enumerate(spamReader):
+				cell_format = wb.add_format({})
+				if len(row) > 8 :
+					taxonomy = row[9] # col J
+					if regex.match(taxonomy) != None:
+						cell_format = wb.add_format({"bg_color":"#CCFFFF"}) # row color as blue if Viruses
+						if row[0] not in  blast_query_id_list:
+							cell_format = wb.add_format({"bg_color":"#FF6347"}) # row color as red if pfam_query_id not in blast_query_id
+					else:
+						cell_format = wb.add_format({})
+				ws.write_row(rowx,0, row, cell_format)
+			ws.autofilter('A1:M1')
+	wb.close()
+
+
+def _set_options ():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-b','--blast',help='A csv Blast file.',action='append',required=True,type=str,dest='blast_files')
+	parser.add_argument('-r','--rps',help='A csv pfam file.',action='append',required=False,type=str,dest='rps_file')
+	parser.add_argument('-o','--out',help='The output file.',action='store',type=str,default='./',dest="out")
+	parser.add_argument('-v','--verbosity',help='Verbose level', action='store',type=int,choices=[1,2,3,4],default=1)
+	args = parser.parse_args()
+	return args
+
+def _set_log_level(verbosity):
+	if verbosity == 1:
+		log_format = '%(asctime)s %(levelname)-8s %(message)s'
+		log.basicConfig(level=log.INFO,format=log_format)
+	elif verbosity == 3:
+		log_format = '%(filename)s:%(lineno)s - %(asctime)s %(levelname)-8s %(message)s'
+		log.basicConfig(level=log.DEBUG,format=log_format)
+
+
+if __name__ == "__main__":
+	main()
